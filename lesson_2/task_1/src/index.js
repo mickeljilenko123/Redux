d873a6f5b2d65242c6423953254dc6466e952dd3
mickeljilenko123 <mickeljilenko@gmail.com>
2021-02-05 20:59:33 +0300

    init

--- lesson_2/task_1/src/index.js
+++ lesson_2/task_1/src/index.js
@@ -27,4 +27,4 @@ const counterReducer = (state = 0, action) => {
 const store = createStore(counterReducer);
 store.dispatch(increment());
 store.dispatch(decrement());
-console.log(store);
+console.log(store.getState());
---
+++ lesson_2/task_1/src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App';
+import './index.js'
+
+const rootElement = document.querySelector("#root");
+
+
+ReactDOM.render(<App />, rootElement);
