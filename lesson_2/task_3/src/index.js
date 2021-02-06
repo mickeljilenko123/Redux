384813bd6e8b940f485ebe727d3f43c669d3a372
mickeljilenko123 <mickeljilenko@gmail.com>
2021-02-06 03:17:11 +0300

    init

--- lesson_2/task_3/src/index.js
+++ lesson_2/task_3/src/index.js
@@ -1,9 +1,9 @@
-// import store from './store'
-// import { addUser, deleteUser } from './user.actions'
+import store from './store'
+import { addUser, deleteUser } from './user.actions'
 
-// store.dispatch(addUser({ id: 34, name: 'Tom' }));
-// store.dispatch(deleteUser(34));
-// console.log(store.getState());
-// store.subscribe(() => {
-//     console.log(store.getState());
-// })
+store.dispatch(addUser({ id: 34, name: 'Tom' }));
+store.dispatch(deleteUser(34));
+console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+})
