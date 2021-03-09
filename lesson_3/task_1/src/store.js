58de8bc07cd42fd4edd70e4249b4179dac41ac28
mickeljilenko123 <mickeljilenko@gmail.com>
2021-03-09 03:21:53 +0300

    init

--- lesson_3/task_1/src/store.js
+++ lesson_3/task_1/src/store.js
@@ -5,5 +5,9 @@ const appReducer = combineReducers({
     counter: counterReducer,
     users: usersReducer,
 })
-const store = createStore(appReducer);
+const store = createStore(
+    appReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 export default store;
