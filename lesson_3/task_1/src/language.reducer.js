d2339117b1f683796bd05d01b6dd64864b9dd081
mickeljilenko123 <mickeljilenko@gmail.com>
2021-03-09 16:01:20 +0300

    init

--- lesson_3/task_1/src/language.reducer.js
+++ lesson_3/task_1/src/language.reducer.js
@@ -2,7 +2,7 @@ import { SET_LANGUAGE } from './language.actions'
 const languageReducer = (state = '', action) => {
     switch (action.type) {
         case SET_LANGUAGE:
-            return action.payload.language;
+            return state + action.payload.language;
         default:
             return state;
     }
